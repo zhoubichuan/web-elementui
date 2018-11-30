7022d740312903bc8f056478a2b0f036467b7f77
“huipaodejituigit config --global user.email 920210993@qq.comgit config --global user.name “huipaodejitui <920210993@qq.com>
2018-11-30 22:50:08 +0800

    更新内容

--- 2.十种跨域方式/1.free-fall/eg/common/commonRequest.js
+++ 2.十种跨域方式/1.free-fall/eg/common/commonRequest.js
@@ -42,19 +42,24 @@ var GlobalAjax = function() {
 var globalAjax = new GlobalAjax();
 
 var globalRequest = {
+  //查询全部
   getRequest: function(success, error) {
-    globalAjax.get("http://localhost:3000/", success, error);
+    globalAjax.get("http://localhost:3000/queryAll", success, error);
   },
+  //查询单个
   queryRequest: function(data, success, error) {
-    globalAjax.post(data, "http://localhost:3000/", success, error);
+    globalAjax.post(data, "http://localhost:3000/querySingle", success, error);
   },
+  //添加
   postRequest: function(data, success, error) {
-    globalAjax.post(data, "http://localhost:3000/", success, error);
+    globalAjax.post(data, "http://localhost:3000/addSingle", success, error);
   },
+  //删除
   deleteRequest: function(data, success, error) {
-    globalAjax.delete(data, "http://localhost:3000/", success, error);
+    globalAjax.delete(data, "http://localhost:3000/deleteSingle", success, error);
   },
+  //更新
   putRequest: function(data, success, error) {
-    globalAjax.put(data, "http://localhost:3000/", success, error);
+    globalAjax.put(data, "http://localhost:3000/update", success, error);
   }
 };
--- 2.十种跨域方式/1.free-fall/eg/server/server.js
+++ 2.十种跨域方式/1.free-fall/eg/server/server.js
@@ -13,71 +13,108 @@ let result = {
     { sort: "4", name: "小李", math: 90, english: 60 }
   ]
 };
-server.on("request", function(req, res) {
-  switch (req.method) {
-    case "GET":
+let query = (req, req, url, method, result, callback) => {
+  if (req.url.includes(type)) {
+    console.log("请求的路径：" + req.url);
+    if (req.method === method) {
+      console.log("请求的方式：" + req.method);
+      callback && callback();
       res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
       res.write(JSON.stringify(result));
       res.end();
+    }
+  }
+};
+let common={
+  //查询全部
+  queryAll: (res, req, result)=>{
+    query(res,res,'queryAll','GET',result,callback)
+  },
+  //查询单个
+  querySingle: (res, req, result, callback) => {
+    let data = "";
+    req.on("data", chunk=> {
+      data += chunk;
+    });
+    req.on("end", ()=> {
+      query(res, req, 'querySingle', 'POST', result, callback)
+    })
+  },
+  //添加单个
+  addSingle: (res, req, result, callback) => {
+    let data = "";
+    req.on("data", chunk => {
+      data += chunk;
+    });
+    req.on("end", () => {
+      query(res, req, 'addSingle', 'POST', result, callback)
+    })
+    
+  },
+  //删除单个
+  deleteSingle: (res, req, result, callback) => {
+    let data = "";
+    req.on("data", chunk => {
+      data += chunk;
+    });
+    req.on("end", () => {
+      query(res, req, 'deleteSingle', 'POST', result, callback)
+    })
+    
+  },
+  //更新
+  update: (res, req, result, callback) => {
+    let data = "";
+    req.on("data", chunk => {
+      data += chunk;
+    });
+    req.on("end", () => {
+      query(res, req, 'update', 'POST', result, callback)
+    })
+    
+  },
+
+}
+server.on("request", function(req, res) {
+  let url = /\/(\w)+\?/g.exec(req.url)[0].slice(1, -1);
+  switch (req.url) {
+    case "GET":
+      common.queryAll(res, req, result)
       break;
     case "POST":
-      var data = "";
-      req.on("data", function(chunk) {
-        data += chunk;
-      });
-      req.on("end", function() {
+      common.querySingle(res, req, result,()=>{
         data = querystring.parse(data.toString());
         if (data.math) {
           data.sort = +result.data[result.data.length - 1].sort + 1;
           result.data.push(data);
         } else {
-          var result_ = JSON.parse(JSON.stringify(result));
-          result_.data = result.data.filter(val => {
+          result = JSON.parse(JSON.stringify(result));
+          result.data = result.data.filter(val => {
             return data.name === val.name;
           });
         }
-
-        res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
-        res.write(JSON.stringify(result_));
-        res.end();
-      });
+      })
       break;
     case "PUT":
-      var data = "";
-      req.on("data", function(chunk) {
-        data += chunk;
-      });
-
-      req.on("end", function() {
+      common.update(res, req, result, ()=>{
         data = querystring.parse(data.toString());
         result.data = result.data.map(val => {
           if (data.name == val.name) {
-            data.sort = result.data[result.data.length - 1].sort + 1;
+            data.sort = +result.data[result.data.length - 1].sort + 1;
             return data;
           } else {
             return val;
           }
         });
-        res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
-        res.write(JSON.stringify(result));
-        res.end();
-      });
+      })
       break;
     case "DELETE":
-      var data = "";
-      req.on("data", function(chunk) {
-        data += chunk;
-      });
-      req.on("end", function() {
+      common.deleteSingle(res, req, result, ()=>{
         data = querystring.parse(data.toString());
-
         result.data = result.data.filter(val => {
           return data.name !== val.name;
         });
-        res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
-        res.write(JSON.stringify(result));
-        res.end();
-      });
+      })
       break;
   }
 });
