b41c559d3b8c6eecbd465124480cd75dcfb201b5
huipaodejitui <920210993@qq.com>
2018-12-24 18:49:27 +0800

    完善知识点

--- 2.十种跨域方式/4.http-proxy-middleware/proxy-middleware.js
+++ 2.十种跨域方式/4.http-proxy-middleware/proxy-middleware.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const proxyMiddleWare = require("http-proxy-middleware");
 
-const proxy = proxyMiddleWare("/", {
-  target: "http://localhost:3000",
-  changeOrigin: true
-});
-app.use("/*", proxy);
+app.use(
+  proxyMiddleWare("/*", {
+    target: "http://localhost:3000",
+    changeOrigin: true
+  })
+);
 
 app.listen(4000, () => {
   console.log("监听4000端口");
---
+++ ajax知识点.md
@@ -0,0 +1,25 @@
+# ajax 知识点
+
+## 1.ajax 基础知识
+
+## 2.十种跨域方式
+
+### 1.降级浏览器跨域
+
+### 2.cors 跨域
+
+### 3.nginx 代理跨域
+
+### 4.http-proxy-middleware
+
+### 5.jsonp 利用 script/img 标签实现跨域
+
+### 6.domain 实现子域名跨域
+
+### 7.name 实现子域名跨域
+
+### 8.hash 实现子域名跨域
+
+### 9.postMessage
+
+### 10.webSocket
