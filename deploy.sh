2622364b3e62b439473f8926167e1a858c5194c3
zhoubichuan <zhoubichuan@icloud.com>
2021-10-08 23:44:23 +0800

    master

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,23 @@ git init
 git checkout --orphan gh-pages
 git add .
 git commit -m 'gh-pages'
+git remote add origin https://gitee.com/zhoubichuan/web-elementui.git
+git push -f origin gh-pages
+
+# 5.返回初始目录
+cd -
+
+# 2.打包
+npm run src:build
+
+# 3.进入打包目录
+cd dist
+
+# 4.发布到gh-pages分支
+git init
+git checkout --orphan gh-pages
+git add .
+git commit -m 'gh-pages'
 git remote add origin https://github.com/zhoubichuan/web-elementui.git
 git push -f origin gh-pages
 
