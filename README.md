071c6eea84c11780f7b1f25b431ca4498fb606f8
zhoubichuan <zhoubichuan@icloud.com>
2022-07-14 01:56:09 +0800

    master

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# ElementUI学习笔记
-- ElementUI基础知识
-- ElementUI源码
+# ElementUI 学习笔记
+
+![一.基础知识](./src/.vuepress/public/home.png)
+
+- 基础知识
+  - [一.基础知识](/web-three/base/engine/1.index.html)
+  - [二.基础案例](/web-three/base/project/1.index.html)
+  - [三.综合案例](/web-three/base/vue2.x/1.index.html)
+  - [四.动画案例](/web-three/base/vue3.x/1.index.html)
+- 高级知识
+  - [一.性能优化](/web-three/senior/component/1.index.html)
+  - [二.深入理论](/web-three/senior/typescript/1.index.html)
+  - [三.构建部署](/web-three/senior/deploy/1.index.html)
+- Vue 2.x 源码
+  - [一.Vue/cli2.x](/web-three/source/vue-cli2.x/1.index.html)
+  - [二.Vue 2.x](/web-three/source/vue2.x/1.index.html)
+  - [三.Vue-Router3.x](/web-three/source/vue-router3.x/1.index.html)
+  - [四.Vuex3.x](/web-three/source/vuex3.x/1.index.html)
+- Vue 3.x 源码
+  - [一.Vue/cli3.x](/web-three/source/vue-cli3.x/1.index.html)
+  - [二.Vue 3.x](/web-three/source/vue3.x/1.index.html)
+  - [三.Vue-Router4.x](/web-three/source/vue-router4.x/1.index.html)
+  - [四.Vuex4.x](/web-three/source/vuex4.x/1.index.html)
+- [Vue 相关资料](/web-three/source/vuex4.x/1.index.html)
---
+++ src/.vuepress/nav.js
@@ -0,0 +1,60 @@
+const nav = [{
+    text: 'ElementUI源码解析',
+    items: [
+      {
+        text: '一.Basic部分',
+        link: '/elementui/base/1.index'
+      },
+      {
+        text: '二.Form部分',
+        link: '/elementui/form/1.index'
+      },
+      {
+        text: '三.Data部分',
+        link: '/elementui/data/1.index'
+      },
+      {
+        text: '四.Notice部分',
+        link: '/elementui/notice/1.index'
+      },
+      {
+        text: '五.Navigation部分',
+        link: '/elementui/navigation/1.index'
+      },
+      {
+        text: '六.Others部分',
+        link: '/elementui/others/1.index'
+      }
+    ]
+  },
+  {
+    text: 'Element-Plus源码解析',
+    items: [
+      {
+        text: '一.Basic部分',
+        link: '/element-plus/base/1.index'
+      },
+      {
+        text: '二.Form部分',
+        link: '/element-plus/form/1.index'
+      },
+      {
+        text: '三.Data部分',
+        link: '/element-plus/data/1.index'
+      },
+      {
+        text: '四.Notice部分',
+        link: '/element-plus/notice/1.index'
+      },
+      {
+        text: '五.Navigation部分',
+        link: '/element-plus/navigation/1.index'
+      },
+      {
+        text: '六.Others部分',
+        link: '/element-plus/others/1.index'
+      }
+    ]
+  }
+  ]
+  module.exports = nav
