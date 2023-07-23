bfe18508a5574d6fab0c45bcd3f445c65e1e7f7d
周必川 <zhoubichuan@icloud.com>
2023-07-23 15:42:57 +0800

    update

--- components/layout/src/Layout.vue
+++ components/layout/src/Layout.vue
@@ -1,44 +1,43 @@
 <template>
-  <el-button
-    size="mini"
-    :loading="loading"
-    v-bind="$attrs"
-    v-on="$listeners"
-    @click="handleClick"
-  >
-    <slot></slot>
-  </el-button>
+  <div style="height: 200px">
+    <div class="container1">
+      <div class="main1">
+        <slot name="main"></slot>
+      </div>
+      <div class="left1">
+        <slot name="left"></slot>
+      </div>
+      <div class="right1">
+        <slot name="right"></slot>
+      </div>
+    </div>
+  </div>
 </template>
-<script>
-export default {
-  name: "WebLayout",
-  data() {
-    return {
-      loading: false,
-    };
-  },
-  props: {
-    duration: {
-      type: Number,
-      default: 1000,
-    },
-  },
-  methods: {
-    handleClick() {
-      this.loading = true;
-      let timer = setTimeout(() => {
-        this.loading = false;
-      }, this.duration);
-      this.$once("hook:beforeDestory", () => {
-        clearTimeout(timer);
-      });
-    },
-  },
-};
-</script>
-<style lang="scss">
-.el-icon-loading {
-  position: absolute;
-  left: 5px;
+<style>
+.container1 {
+  padding: 0 200px 0 300px;
+}
+.main1,
+.left1,
+.right1 {
+  min-height: 200px;
+  float: left;
+  position: relative;
+}
+.main1 {
+  background: red;
+  width: 100%;
+}
+.left1 {
+  background: yellow;
+  width: 300px;
+  margin-left: -100%;
+  left: -300px;
+}
+.right1 {
+  background: green;
+  width: 200px;
+  right: -200px;
+  margin-left: -200px;
 }
 </style>
