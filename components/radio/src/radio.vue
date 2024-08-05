179319183e705e3658edc78f58ed0510e2c9b437
看雪 <zhoubichuan@icloud.com>
2024-08-05 20:58:39 +0800

    Update radio.vue

--- components/radio/src/radio.vue
+++ components/radio/src/radio.vue
@@ -1,44 +1,53 @@
 <template>
-  <el-radio
-    size="mini"
-    :loading="loading"
-    v-bind="$attrs"
-    v-on="$listeners"
-    @click="handleClick"
-  >
-    <slot></slot>
-  </el-radio>
+    <el-radio class="web-radio" v-bind="$attrs" v-on="$listeners">
+        <slot></slot>
+    </el-radio>
 </template>
 <script>
 export default {
-  name: "WebRadio",
-  data() {
-    return {
-      loading: false,
-    };
-  },
-  props: {
-    duration: {
-      type: Number,
-      default: 1000,
+    name: "WebRadio",
+    data() {
+        return {};
     },
-  },
-  methods: {
-    handleClick() {
-      this.loading = true;
-      let timer = setTimeout(() => {
-        this.loading = false;
-      }, this.duration);
-      this.$once("hook:beforeDestory", () => {
-        clearTimeout(timer);
-      });
-    },
-  },
+    props: {},
+    methods: {},
 };
 </script>
 <style lang="scss">
-.el-icon-loading {
-  position: absolute;
-  left: 5px;
+.web-radio {
+    margin-right: 29px;
+    &.el-radio {
+        .el-radio__input {
+            .el-radio__inner {
+                width: 18px;
+                height: 18px;
+                border-radius: 100%;
+                margin-bottom: 2px;
+            }
+            &.is-checked {
+                .el-radio__inner {
+                    border: 1px solid #0075ff;
+                    background: none;
+                    &::after {
+                        height: 9px;
+                        width: 9px;
+                        background-color: #0075ff !important; //被全局样式覆盖
+                    }
+                }
+            }
+        }
+        .el-radio__label {
+            font-family: $u3-font-regular;
+            font-weight: 400;
+            color: #244367;
+            padding-left: 5px;
+            font-size: 20px;
+        }
+        .el-radio__input.is-checked {
+            & + .el-radio__label {
+                color: #244367;
+            }
+        }
+    }
 }
 </style>
